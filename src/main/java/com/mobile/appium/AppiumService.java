183f8372028b3893c57534746222357c40d7ec9a
phkadam2008 <phkadam2008@gmail.com>
2016-09-17 12:05:46 +0530

    Added logic to start and destroy webkit_debug_proxy.

--- src/main/java/com/mobile/appium/AppiumService.java
+++ src/main/java/com/mobile/appium/AppiumService.java
@@ -2,11 +2,15 @@ package com.mobile.appium;
 
 import com.core.logger.CustomLogger;
 import com.core.utils.PropertiesReader;
+import com.mobile.os.iOS.iOSDevice;
+import com.mobile.os.iOS.iOSWebKitDebugProxy;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
 import io.appium.java_client.service.local.AppiumServiceBuilder;
 import io.appium.java_client.service.local.flags.AndroidServerFlag;
 import io.appium.java_client.service.local.flags.GeneralServerFlag;
 import io.appium.java_client.service.local.flags.IOSServerFlag;
+import io.appium.java_client.service.local.flags.ServerArgument;
+
 import java.io.File;
 import java.net.URL;
 
@@ -17,8 +21,9 @@ public class AppiumService {
 
     AvailablePort port = new AvailablePort();
     public AppiumDriverLocalService appiumDriverLocalService;
+    String webKitPort = "";
 
-    public AppiumServiceBuilder startAppiumForAndroiOriOSSimulator(String deviceID)
+    public AppiumServiceBuilder startAppiumForAndroidDevice(String deviceID)
             throws Exception {
         System.out.println("**************************************************************************");
         System.out.println("** Starting Appium Service to handle Android Device:: " + deviceID + " ***");
@@ -66,9 +71,12 @@ public class AppiumService {
         return builder;
     }
 
-    public AppiumServiceBuilder startAppiumForiOSDevices(String deviceID, String webKitPort) throws Exception {
+    public AppiumServiceBuilder startAppiumForiOSDevice(iOSDevice iOSDevice) throws Exception {
+        String deviceName = iOSDevice.getName();
+
+
         System.out.println("**********************************************************************");
-        System.out.println("****** Starting Appium Service to handle IOS :: " + deviceID + " *****");
+        System.out.println("****** Starting Appium Service to handle IOS :: " + deviceName + " *****");
         System.out.println("**********************************************************************");
 
         CustomLogger.log.debug("Building Appium service for iOS devices.");
@@ -76,20 +84,32 @@ public class AppiumService {
         String tmpDir = new File(System.getProperty("user.dir")).getAbsolutePath() + "/target/" + "tmp_" + port;
 
         String logFileLocation = System.getProperty("user.dir") + "/target/appiumlogs/" +
-                deviceID.replaceAll("\\W", "_") + ".txt";
+                deviceName.replaceAll("\\W", "_") + ".txt";
 
         File APPIUM_JS_FILE = new File(PropertiesReader.config.getValue("APPIUM_JS_PATH"));
 
-        int port = this.port.getPort();
+
+        int appiumPort = this.port.getPort();
+
+
         AppiumServiceBuilder builder =
                 new AppiumServiceBuilder()
                         .withAppiumJS(APPIUM_JS_FILE)
                         .withArgument(GeneralServerFlag.LOG_LEVEL, "info")
                         .withLogFile(new File(logFileLocation))
-                        .withArgument(IOSServerFlag.WEBKIT_DEBUG_PROXY_PORT, webKitPort)
+//                        .withArgument(IOSServerFlag.WEBKIT_DEBUG_PROXY_PORT, webKitPort)
                         .withArgument(GeneralServerFlag.TEMP_DIRECTORY, tmpDir)
+                        .withArgument(nativeInstruments)
                         .withArgument(GeneralServerFlag.SESSION_OVERRIDE)
-                        .usingPort(port);
+                        .usingPort(appiumPort);
+
+//      Start webkit proxy only for real iOS devices
+        if(iOSDevice.getUdid().length() == 40) {
+            webKitPort = Integer.toString(this.port.getPort());
+            iOSWebKitDebugProxy.start(iOSDevice, webKitPort);
+            builder.withArgument(IOSServerFlag.WEBKIT_DEBUG_PROXY_PORT, webKitPort);
+        }
+
         appiumDriverLocalService = builder.build();
         appiumDriverLocalService.start();
 
@@ -100,11 +120,17 @@ public class AppiumService {
                 " WEBKIT_DEBUG_PROXY_PORT : " + webKitPort +
                 " TEMP_DIRECTORY : " + tmpDir +
                 " SESSION_OVERRIDE" +
-                " PORT : " + port);
+                " PORT : " + appiumPort);
 
         return builder;
 
     }
+
+    ServerArgument nativeInstruments = new ServerArgument() {
+        @Override public String getArgument() {
+            return "--native-instruments-lib";
+        }
+    };
 
     public URL getHost() {
         return appiumDriverLocalService.getUrl();
@@ -115,5 +141,8 @@ public class AppiumService {
             appiumDriverLocalService.stop();
             CustomLogger.log.debug("Destroyed Appium server.");
         }
+
+        if(!webKitPort.isEmpty())
+            iOSWebKitDebugProxy.kill(webKitPort);
     }
 }
