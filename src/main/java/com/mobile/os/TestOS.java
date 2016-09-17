21779e34f78feb85ae51f9cf98616d43c6be8586
phkadam2008 <phkadam2008@gmail.com>
2016-09-17 12:06:54 +0530

    Added test to verify iOSWebKitDebugProxy.kill

--- src/main/java/com/mobile/os/TestOS.java
+++ src/main/java/com/mobile/os/TestOS.java
@@ -1,7 +1,28 @@
 package com.mobile.os;
 
+import com.core.logger.CustomLogger;
+import com.mobile.os.iOS.iOSWebKitDebugProxy;
+import org.apache.log4j.Level;
+import org.testng.annotations.Test;
+
+import java.io.File;
+
 /**
  * Created by pritamkadam on 17/09/16.
  */
 public class TestOS {
+
+    {
+        String usrDir = new File(System.getProperty("user.dir")).getAbsolutePath();
+        System.setProperty("my.log", usrDir + "/target/");
+        CustomLogger.log.setLevel(Level.ALL);
+    }
+
+    @Test
+    public void testWebkitKill(){
+
+
+
+        iOSWebKitDebugProxy.kill(9221 + "");
+    }
 }
